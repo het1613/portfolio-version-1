2d0c0066c9e986d7c28f25c8e3f159708a2c796d
het1613 <het1613@gmail.com>
2021-08-29 13:43:28 -0400

    minor tweaks

--- navbar.js
+++ navbar.js
@@ -3,3 +3,5 @@ window.addEventListener('scroll', function () {
   let windowPosition = window.scrollY > (window.screen.availHeight/15);
   header.classList.toggle('scrolling-active', windowPosition);
 })
+
+// test to see if git is working
